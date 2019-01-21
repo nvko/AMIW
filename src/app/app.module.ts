0cf550652faeb87bebf31f38fb66ebc2a156b9ec
Kredensinh0 <ksawery.hasnik@gmail.com>
2019-01-21 23:54:22 +0100

    project update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PurchaseHistoryComponent } from './purchase-history/purchase-history.component';
+import { ProductShowcaseComponent } from './product-showcase/product-showcase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingCartComponent,
+    PurchaseHistoryComponent,
+    ProductShowcaseComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductShowcaseComponent},
+      { path: 'cart', component: ShoppingCartComponent },
+      { path: 'history', component: PurchaseHistoryComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
